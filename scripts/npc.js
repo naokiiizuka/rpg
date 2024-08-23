f08aa573089fff2fab834ff9fcfdcc1afe57196e
Naoki Iizuka <naokiiizuka@mac.com>
2024-08-23 09:08:13 +0900

    Add king

--- scripts/npc.js
+++ scripts/npc.js
@@ -1,13 +1,16 @@
 import { npcImage } from "./images.js";
 
 export const npcs = [
-  { x: 7, y: 9, frame: 0 },
-  { x: 9, y: 9, frame: 0 },
+  { x: 7, y: 6, frame: 2, anime: 0 },
+  { x: 7, y: 9, frame: 0, anime: 1 },
+  { x: 9, y: 9, frame: 0, anime: 1 },
 ];
 
 export function drawNPCs(ctx, tileSize) {
   npcs.forEach((npc) => {
-    npc.frame = 1 - npc.frame;
+    if (npc.anime) {
+      npc.frame = 1 - npc.frame;
+    }
     const npcTileX = npc.frame;
     const npcTileY = 0;
     ctx.drawImage(npcImage, npcTileX * tileSize, npcTileY * tileSize, tileSize, tileSize, npc.x * tileSize, npc.y * tileSize, tileSize, tileSize);
